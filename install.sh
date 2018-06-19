c1c62c87bddd514dea7c5541a26975ccb2f3d511
dangjiahe <dangjiahe@cvte.com>
2018-06-19 15:17:45 +0800

    [feature] 第一次提交

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+sudo pacman -S vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sudo pacman -S zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
